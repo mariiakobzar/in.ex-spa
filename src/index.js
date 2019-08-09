c77e0e97ad3692b95151b0dab4c5f0d6124aa881
mariiakobzar <mariia.kobzar@gmail.com>
2019-08-09 18:22:45 +0300

    + fontawesome

--- src/index.js
+++ src/index.js
@@ -1,50 +1,169 @@
-import Router from './services/router';
+import './services/router';
 import './assets/styles/styles.scss';
 
-const contentDiv = document.getElementById('app');
-const homeLinks = document.getElementsByClassName('home-link');
-const aboutLinks = document.getElementsByClassName('about-link');
-const shopLinks = document.getElementsByClassName('shop-link');
-const forumLinks = document.getElementsByClassName('forum-link');
-const contactLinks = document.getElementsByClassName('contact-link');
 
-function onNavItemClick(pathName) {
-  window.history.pushState({}, pathName, window.location.origin + pathName);
-  contentDiv.innerHTML = Router[pathName];
-}
+/* global jQuery */
 
-window.onpopstate = () => {
-  contentDiv.innerHTML = Router[window.location.pathname];
-};
+(($) => {
+  $(document).ready(() => {
+    const maxWidth = window.matchMedia('(max-width: 480px)');
 
-contentDiv.innerHTML = Router[window.location.pathname];
+    function checkWidth(width) {
+      if (width.matches) {
+        $('.burger').removeClass('visually-hidden').css('position', 'relative');
+      } else {
+        $('.burger').addClass('visually-hidden').css('position', 'fixed');
+      }
+    }
 
-Array.from(homeLinks).forEach((el) => {
-  el.addEventListener('click', () => {
-    onNavItemClick('/');
-  }, false);
-});
+    checkWidth(maxWidth);
+    maxWidth.addListener(checkWidth);
 
-Array.from(shopLinks).forEach((el) => {
-  el.addEventListener('click', () => {
-    onNavItemClick('/shop');
-  }, false);
-});
+    $('.burger').click(() => {
+      $('.burger-menu').toggleClass('visually-hidden');
+      $('.burger').toggleClass('burger--open');
+    });
 
-Array.from(aboutLinks).forEach((el) => {
-  el.addEventListener('click', () => {
-    onNavItemClick('/about');
-  }, false);
-});
+    $('.burger-menu__item').click(() => {
+      $('.burger-menu').toggleClass('visually-hidden');
+      $('.burger').toggleClass('burger--open');
+    });
 
-Array.from(forumLinks).forEach((el) => {
-  el.addEventListener('click', () => {
-    onNavItemClick('/forum');
-  }, false);
-});
+    $('main').on('click', '.filter-head', () => {
+      $(this).next().toggleClass('visually-hidden');
+      $(this).children('.plus').toggleClass('active');
+    });
 
-Array.from(contactLinks).forEach((el) => {
-  el.addEventListener('click', () => {
-    onNavItemClick('/contact');
-  }, false);
-});
+    $('.basket-content').click(() => {
+      $('.cart').toggleClass('visually-hidden');
+      $('.cart-btn').toggleClass('active');
+      $('.cart-card').toggleClass('active');
+    });
+
+    $('.cart-btn').click(() => {
+      $('.cart-btn').toggleClass('active');
+      setTimeout(() => {
+        $('.cart-card').toggleClass('active');
+      }, 400);
+      setTimeout(() => {
+        $('.cart').toggleClass('visually-hidden');
+      }, 850);
+    });
+
+    function openQuickView() {
+      const srcImg = $(this).siblings('img').attr('src');
+      const title = $(this).parent().parent().find('.item-info__name')
+        .text();
+      const price = $(this).parent().parent().find('.item-info__price')
+        .text();
+
+      $('.q-view').removeClass('visually-hidden');
+      $('.q-view-card__img img').attr('src', srcImg);
+      $('.q-view-card-info__title').text(title);
+      $('.q-view-card-info__price').text(price);
+    }
+
+    $('main').on('click', '.item--hover', openQuickView);
+
+    $('.q-view').on('click', (e) => {
+      if (e.target === $('#close-btn-q')[0] || e.target === $('.q-view')[0]) {
+        $('.q-view').addClass('visually-hidden');
+      }
+    });
+
+    function sortByNewest() {
+      const items = $('.products-container .item');
+      const arItems = $.makeArray(items);
+      arItems.sort((a, b) => {
+        const f = a;
+        const s = b;
+        return parseInt($(f).data('id'), 10) - parseInt($(s).data('id'), 10);
+      });
+      $(arItems).appendTo('.products-container');
+    }
+
+    function sortByPriceFromLow() {
+      const items = $('.products-container .item');
+      const arItems = $.makeArray(items);
+      arItems.sort((a, b) => {
+        const f = a;
+        const s = b;
+        return parseFloat($(f).data('price').substr(1), 10) - parseFloat($(s).data('price').substr(1), 10);
+      });
+      $(arItems).appendTo('.products-container');
+    }
+
+    function sortByPriceFromHigh() {
+      const items = $('.products-container .item');
+      const arItems = $.makeArray(items);
+      arItems.sort((a, b) => {
+        const f = a;
+        const s = b;
+        return parseFloat($(s).data('price').substr(1), 10) - parseFloat($(f).data('price').substr(1), 10);
+      });
+      $(arItems).appendTo('.products-container');
+    }
+
+    function sortByNameFromA() {
+      const items = $('.products-container .item');
+      const arItems = $.makeArray(items);
+      arItems.sort((a, b) => {
+        let f = a;
+        let s = b;
+        [f] = [$(f).data('name').split(' ')[0]];
+        [s] = [$(s).data('name').split(' ')[0]];
+        if (f < s) {
+          return -1;
+        }
+        if (f > s) {
+          return 1;
+        }
+        return 0;
+      });
+      $(arItems).appendTo('.products-container');
+    }
+
+    function sortByNameFromZ() {
+      const items = $('.products-container .item');
+      const arItems = $.makeArray(items);
+      arItems.sort((a, b) => {
+        let f = a;
+        let s = b;
+        [f] = [$(a).data('name').split(' ')[0]];
+        [s] = [$(b).data('name').split(' ')[0]];
+        if (f > s) {
+          return -1;
+        }
+        if (f < s) {
+          return 1;
+        }
+        return 0;
+      });
+      $(arItems).appendTo('.products-container');
+    }
+
+    function checkOption() {
+      switch ($('#select-sort option:selected').text()) {
+        case 'Newest':
+          sortByNewest();
+          break;
+        case 'Price (low to high)':
+          sortByPriceFromLow();
+          break;
+        case 'Price (high to low)':
+          sortByPriceFromHigh();
+          break;
+        case 'Name A-Z':
+          sortByNameFromA();
+          break;
+        case 'Name Z-A':
+          sortByNameFromZ();
+          break;
+        default:
+          break;
+      }
+    }
+
+    $('main').on('change', '#select-sort', checkOption);
+  });
+})(jQuery);
